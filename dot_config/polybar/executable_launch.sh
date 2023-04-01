44065b459df18f727dab6351aee30a05a24222c1
Markus T <19794318+marcus-wishes@users.noreply.github.com>
2023-04-01 22:01:24 +0200

    initial

---
+++ dot_config/polybar/executable_launch.sh
@@ -0,0 +1,11 @@
+#!/run/current-system/sw/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top-internal &
+done
